ba5427654fad6ed17423eb0674062380c03dbbaa
Guangze <guangze@iappsasia.com>
2022-10-09 22:47:41 +0800

    Work queues

---
+++ Work queues/worker.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+
+$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+$channel = $connection->channel();
+
+$channel->queue_declare('hello', false, false, false, false);
+
+echo " [*] Waiting for messages. To exit press CTRL+C\n";
+
+$callback = function ($msg) {
+    echo ' [✔] Received ', $msg->body, "\n";
+    sleep(substr_count($msg->body, '.'));
+    echo " [✔] Done\n";
+};
+
+$channel->basic_consume('hello', '', false, true, false, false, $callback);
+
+while ($channel->is_open()) {
+    $channel->wait();
+}

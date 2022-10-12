bb2aeb71ce22fc55b1653a9d17d96c4cc319f232
Guangze <guangze@iappsasia.com>
2022-10-12 19:31:12 +0800

    Persist messages

--- Hello world/send.php
+++ Hello world/send.php
@@ -8,9 +8,12 @@ use PhpAmqpLib\Message\AMQPMessage;
 $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
 $channel = $connection->channel();
 
-$channel->queue_declare('hello', false, false, false, false);
+$channel->queue_declare('hello', false, true, false, false);
 
-$msg = new AMQPMessage('Hello World!');
+$msg = new AMQPMessage(
+    'Hello World!',
+    array('delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT)
+);
 $channel->basic_publish($msg, '', 'hello');
 
 echo " [✔] Sent 'Hello World!'\n";
--- Work queues/new_task.php
+++ Work queues/new_task.php
@@ -8,13 +8,16 @@ use PhpAmqpLib\Message\AMQPMessage;
 $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
 $channel = $connection->channel();
 
-$channel->queue_declare('hello', false, false, false, false);
+$channel->queue_declare('hello', false, true, false, false);
 
 $data = implode(' ', array_slice($argv, 1));
 if (empty($data)) {
     $data = "Hello World!";
 }
-$msg = new AMQPMessage($data);
+$msg = new AMQPMessage(
+    $data,
+    array('delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT)
+);
 
 $channel->basic_publish($msg, '', 'hello');
 
